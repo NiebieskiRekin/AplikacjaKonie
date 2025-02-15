4262071ab7ca80b9ef31a7129c7d85dd96226a40
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-02-15 19:31:11 +0100

    app: update config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -5,13 +5,17 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["**/dist"] },
   {
     extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
