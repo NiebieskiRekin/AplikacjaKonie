28077d06091b993ad97bd9f04e33e32855d62433
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-02-18 22:01:53 +0100

    backend: simplify paths

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,11 +1,22 @@
 // src/index.ts
 import express, { Express, Request, Response } from "express";
-import { PORT } from "@/env";
+import { PORT } from "./env";
+import { db } from "./db"
+import {konie} from "./db/schema"
 
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  db.select().from(konie).then((value) => {
+    const r = JSON.stringify(value);
+    res.send("Express + TypeScript Server "+ r);
+    console.log('Promise resolved with value: ' + r);
+  })
+  .catch((error) => {
+    res.send("Express + TypeScript Server+  db error");
+    console.error('Promise rejected with error: ' + error);
+  });
+  
 });
 
 app.listen(PORT, () => {
