7d996d9501dbd788c8101717b1c3a05ab1f4f6c7
Jakub <komputer414@gmail.com>
2025-02-25 00:08:19 +0100

    dodanie Navbar`u

---
+++ app/frontend/src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-green-800 text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo po lewej */}
+        <h1 className="text-xl font-bold">
+          <Link to="/">🐴 Moja Hodowla</Link>
+        </h1>
+
+        <div className="flex-1 flex justify-center gap-18">
+          <Link
+            to="/konie"
+            className="px-6 py-3 rounded-lg shadow-lg bg-gradient-to-r from-green-400 to-green-600 text-white font-semibold hover:bg-opacity-80 transition"
+          >
+            Konie
+          </Link>
+          <Link
+            to="/weterynarze"
+            className="px-6 py-3 rounded-lg shadow-lg bg-gradient-to-r from-blue-400 to-blue-600 text-white font-semibold hover:bg-opacity-80 transition"
+          >
+            Weterynarze
+          </Link>
+          <Link
+            to="/kowale"
+            className="px-6 py-3 rounded-lg shadow-lg bg-gradient-to-r from-yellow-400 to-yellow-600 text-white font-semibold hover:bg-opacity-80 transition"
+          >
+            Kowale
+          </Link>
+          <Link
+            to="/wydarzenia"
+            className="px-6 py-3 rounded-lg shadow-lg bg-gradient-to-r from-red-400 to-red-600 text-white font-semibold hover:bg-opacity-80 transition"
+          >
+            Wydarzenia w stajni
+          </Link>
+        </div>
+
+        <button
+          onClick={() => {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+          }}
+          className="hover:text-brown-300 transition font-semibold"
+        >
+          Wyloguj
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
