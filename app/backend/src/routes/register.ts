df4e850c6415da873b1f043ef381525db38267a1
root <root@DESKTOP-0O83CRG.>
2025-02-24 21:28:35 +0100

    dodanie edycji permisji dla uzytkownikow

--- app/backend/src/routes/register.ts
+++ app/backend/src/routes/register.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import bcrypt from "bcrypt";
-import { users, usersInsertSchema } from "../db/schema";
+import { users, usersInsertSchema, userPermissionsInsertSchema, user_permissions } from "../db/schema";
 import { ProcessEnv } from "../env";
 import { db } from "../db";
 import { basicAuth } from 'hono/basic-auth';
@@ -29,6 +29,19 @@ const register = new Hono()
   } catch (error) {
     return c.json({ error: error }, 500);
   }
-});
+}).post("/permission", zValidator("json", userPermissionsInsertSchema), 
+  async (c) => {
+    try {
+      var user_permission = c.req.valid("json");
+      const [added_permission] = await db.insert(user_permissions).values(user_permission).returning();
+      if (!added_permission) {
+        return c.json({ error: "Błąd dodawania użytkownika." }, 401);
+      }
+  
+      return c.json({ added_permission });
+    } catch (error) {
+      return c.json({ error: error }, 500);
+    }
+  });
 
 export default register;
