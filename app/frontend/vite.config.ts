943538d72f5e518b739393fa9a6428c76e1e43de
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-02-24 14:44:00 +0100

    front: migrate vite to vitePWA and add React router v7

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,8 +1,38 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [tailwindcss(),react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'aplikacja-konie-frontend',
+      short_name: 'aplikacja-konie-frontend',
+      description: 'Aplikacja dla hodowców koni i weterynarzy',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
