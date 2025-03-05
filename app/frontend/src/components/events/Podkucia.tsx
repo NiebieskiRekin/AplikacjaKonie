c7b9600a43f01b440cfd2c89b25026c9d0145d14
kuball1000 <jk.buler@gmail.com>
2025-03-05 23:24:29 +0100

    dodanie poszeglownych wydarzen per kon frontend

---
+++ app/frontend/src/components/events/Podkucia.tsx
@@ -0,0 +1,5 @@
+import HorseEventList from "../HorseEventList";
+function Podkucia() {
+  return <HorseEventList type="podkucia" />;
+}
+export default Podkucia;
---
+++ app/frontend/src/components/events/Profilaktyczne.tsx
@@ -0,0 +1,5 @@
+import HorseEventList from "../HorseEventList";
+function Profilaktyczne() {
+  return <HorseEventList type="zdarzenia_profilaktyczne" />;
+}
+export default Profilaktyczne;
