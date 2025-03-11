b0c23066b472295df49e022bd873bca49f9dbb46
kuball1000 <jk.buler@gmail.com>
2025-03-11 16:07:09 +0100

    dodanie /ustawienia do routes na backend

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -8,6 +8,7 @@ import { hodowcyKoniRoute } from "./hodowcykoni";
 import refresh from "./refresh";
 import kowaleRoute from "./kowale";
 import weterynarzeRoute from "./weterynarze";
+import settingsRoute from "./settings";
 
 export function registerRoutes(app: Hono) {
   return app
@@ -20,7 +21,8 @@ export function registerRoutes(app: Hono) {
     .route("/wydarzenia", wydarzeniaRoute)
     .route("/hodowcykoni", hodowcyKoniRoute)
     .route("/kowale", kowaleRoute)
-    .route("/weterynarze", weterynarzeRoute);
+    .route("/weterynarze", weterynarzeRoute)
+    .route("/ustawienia", settingsRoute)
 }
 export const apiRoutes = registerRoutes(new Hono());
 
