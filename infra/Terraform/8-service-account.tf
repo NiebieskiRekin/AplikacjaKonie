ed9856003018d01ef17c25fad69d65b6e5cf8b2c
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-03-12 18:21:59 +0100

    infra: temporarily disable fcm permission for service account

--- infra/Terraform/8-service-account.tf
+++ infra/Terraform/8-service-account.tf
@@ -11,11 +11,11 @@ resource "google_project_iam_member" "storage_uploader_binding" {
 }
 
 // Firebase Cloud messaging
-resource "google_project_iam_member" "fcm_admin_binding" {
-  project = local.project_id
-  role    = "roles/cloudmessaging.messages.create"
-  member  = "serviceAccount:${google_service_account.image-uploader-and-notifier.email}"
-}
+# resource "google_project_iam_member" "fcm_admin_binding" {
+#   project = local.project_id
+#   role    = "roles/cloudmessaging.messages.create"
+#   member  = "serviceAccount:${google_service_account.image-uploader-and-notifier.email}"
+# }
 
 resource "google_service_account_key" "image-uploader-and-notifier-key" {
   service_account_id = google_service_account.image-uploader-and-notifier.name
