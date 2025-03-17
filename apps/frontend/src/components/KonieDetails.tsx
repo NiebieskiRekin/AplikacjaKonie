29b8632d8bec787bd573d6b2990140ff28097cb0
kuball1000 <jk.buler@gmail.com>
2025-03-17 18:12:59 +0100

    dodanie przycisku cofania do panelu wszystkich koni z widoku danego konia

--- apps/frontend/src/components/KonieDetails.tsx
+++ apps/frontend/src/components/KonieDetails.tsx
@@ -178,8 +178,18 @@ function KonieDetails() {
 
   return (
     <div className="to-brown-600 flex min-h-screen flex-col items-center bg-gradient-to-br from-green-800 p-6">
-      <h2 className="mb-6 text-3xl font-bold text-white">{horse.nazwa}</h2>
+      <div className="flex w-full max-w-7xl items-center justify-center relative mb-10 sm:mb-6">
+        <button
+          onClick={() => navigate("/konie")}
+          className="absolute left-0 sm:relative sm:mr-auto flex items-center gap-2 text-white bg-gradient-to-r from-gray-500 to-gray-700 px-4 py-2 rounded-lg transition"
+        >
+          <GoArrowLeft className="text-xl" />
+        </button>
 
+        <h2 className="text-3xl font-bold text-white text-center absolute left-1/2 transform -translate-x-1/2">
+          {horse.nazwa}
+        </h2>
+      </div>
       <div className="flex w-full max-w-7xl flex-col gap-6 rounded-lg bg-white p-6 shadow-lg md:flex-row">
         <div className="flex-1">
           <h3 className="mb-4 text-xl font-bold text-green-900">
