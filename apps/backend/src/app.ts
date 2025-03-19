c01a7d938c8caa45ac4198bfed875085ceb49cfb
kuball1000 <jk.buler@gmail.com>
2025-03-19 20:24:22 +0100

    dodanie logow

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -3,12 +3,14 @@ import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import { ProcessEnv } from "./env";
 import { registerRoutes } from "./routes";
+import { log } from "./logs/logger";
 
 const app = registerRoutes(new Hono());
 
 app.use("/api/*", cors());
 if (ProcessEnv.NODE_ENV != "production") {
   app.use("*", logger()); // Only for testing and development
+  log("Server", "info", "Połączono z Serwerem");
 }
 
 export default app;
