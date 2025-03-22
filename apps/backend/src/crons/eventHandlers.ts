6c9a76c5beea8cb27d5f54064e2d47ae1f87b100
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-03-22 22:44:33 +0100

    backend: refactor email notifications query

--- apps/backend/src/crons/eventHandlers.ts
+++ apps/backend/src/crons/eventHandlers.ts
@@ -2,33 +2,64 @@ import { db } from "../db";
 import { and, eq, gte, sql, or, isNull, isNotNull, lte } from "drizzle-orm";
 import { notifications, podkucia, konie, users, zdarzeniaProfilaktyczne } from "../db/schema";
 
-
-export function subtractDays(dateStr: string, days: number): string {
-  const date = new Date(dateStr);
-  date.setDate(date.getDate() - days);
-  return date.toISOString().split("T")[0];
-}
-
 /**
  * Pobiera wydarzenia użytkownika na podstawie ustawień powiadomień
  */
 export async function fetchUserEvents() {
-  const now = new Date();
-  const today = now.toISOString().split("T")[0];
-  const currentHour = now.getHours().toString().padStart(2, "0") + ":00";
-
-  const _notifications = await db
-    .select({
+  const _notifications = db.$with('users_notifications').as(
+    db.select({
       userId: users.id,
       hodowlaId: users.hodowla,
       rodzajZdarzenia: notifications.rodzajZdarzenia,
       rodzajWysylania: notifications.rodzajWysylania,
       time: notifications.time,
       days: notifications.days,
+      email: users.email
     })
     .from(notifications)
     .innerJoin(users, eq(notifications.userId, users.id))
-    .where(or(eq(notifications.rodzajWysylania, "Email"), eq(notifications.rodzajWysylania, "Oba")));
+    .where(or(
+      eq(notifications.rodzajWysylania, "Email"),
+      eq(notifications.rodzajWysylania, "Oba"),
+    )));
+
+  const _events = db.$with('events').as(db.select({
+          id: podkucia.id, 
+          dataWaznosci: podkucia.dataWaznosci,
+          rodzajZdarzenia: sql<string>`'Podkucia'`.as("rodzajZdarzenia"),
+          kon: podkucia.kon
+        }).from(podkucia)
+        .union(
+          db.select({
+            id: zdarzeniaProfilaktyczne.id,
+            dataWaznosci: zdarzeniaProfilaktyczne.dataWaznosci,
+            rodzajZdarzenia: zdarzeniaProfilaktyczne.rodzajZdarzenia,
+            kon: zdarzeniaProfilaktyczne.kon
+          }).from(zdarzeniaProfilaktyczne)
+        ));
+
+      const _upcoming_events = await db.with(_events,_notifications).selectDistinct({
+          id: _events.id,
+          dataWaznosci: _events.dataWaznosci,
+          rodzajZdarzenia: _events.rodzajZdarzenia,
+          nazwaKonia: konie.nazwa,
+          rodzajKonia: konie.rodzajKonia,
+          email: users.email,
+      }).from(_events)
+      .innerJoin(konie, eq(_events.kon, konie.id))
+      .innerJoin(_notifications, eq(konie.hodowla, _notifications.hodowlaId))
+      .where(
+        and(
+          or(
+            gte(_events.dataWaznosci, sql`CURRENT_DATE - ${_notifications.days}`),
+            lte(_events.dataWaznosci, sql`CURRENT_DATE`),
+          ),
+          gte(_notifications.time, sql`DATE_TRUNC('hour',CURRENT_TIMESTAMP)::time`),
+          isNull(konie.dataOdejsciaZeStajni),
+          isNotNull(podkucia.dataWaznosci),
+        )
+      ).orderBy(konie.nazwa, _events.dataWaznosci)
+    
 
     const userNotifications: Record<
     string,
@@ -36,68 +67,8 @@ export async function fetchUserEvents() {
       wydarzenia: Record<string, { nazwaKonia: string; rodzajKonia: string; dataWaznosci: string }[]>;
     }
   > = {};
-// Tutaj na pewno jest do pomyślenia, żeby to zoptymalizować, 
-// jak mi zaczeło działać to nie chciałem w tym grzebać, bo trochę z tym się bawiłem...
-  for (const setting of _notifications) {
-    const { rodzajZdarzenia, hodowlaId } = setting;
-    let events = [];
 
-    if (rodzajZdarzenia === "Podkucia") {
-      events = await db
-        .selectDistinct({
-          id: podkucia.id,
-          dataWaznosci: podkucia.dataWaznosci,
-          rodzajZdarzenia: sql<string>`'Podkucia'`.as("rodzajZdarzenia"),
-          nazwaKonia: konie.nazwa,
-          rodzajKonia: konie.rodzajKonia,
-          email: users.email,
-        })
-        .from(podkucia)
-        .innerJoin(konie, eq(podkucia.kon, konie.id))
-        .innerJoin(users, eq(konie.hodowla, users.hodowla))
-        .where(
-          and(
-            eq(konie.hodowla, hodowlaId),
-            or(
-              gte(podkucia.dataWaznosci, subtractDays(today, Number(setting.days))),
-              lte(podkucia.dataWaznosci, today)
-            ),
-            gte(sql`${setting.time}`, currentHour),
-            isNull(konie.dataOdejsciaZeStajni),
-            isNotNull(podkucia.dataWaznosci),
-          )
-        )
-        .orderBy(konie.nazwa, podkucia.dataWaznosci);
-    } else {
-      events = await db
-        .selectDistinct({
-          id: zdarzeniaProfilaktyczne.id,
-          dataWaznosci: zdarzeniaProfilaktyczne.dataWaznosci,
-          rodzajZdarzenia: zdarzeniaProfilaktyczne.rodzajZdarzenia,
-          nazwaKonia: konie.nazwa,
-          rodzajKonia: konie.rodzajKonia,
-          email: users.email,
-        })
-        .from(zdarzeniaProfilaktyczne)
-        .innerJoin(konie, eq(zdarzeniaProfilaktyczne.kon, konie.id))
-        .innerJoin(users, eq(konie.hodowla, users.hodowla))
-        .where(
-          and(
-            eq(zdarzeniaProfilaktyczne.rodzajZdarzenia, rodzajZdarzenia),
-            eq(konie.hodowla, hodowlaId),
-            or(
-              gte(zdarzeniaProfilaktyczne.dataWaznosci, subtractDays(today, Number(setting.days))),
-              lte(zdarzeniaProfilaktyczne.dataWaznosci, today)
-            ),
-            gte(sql`${setting.time}`, currentHour),
-            isNull(konie.dataOdejsciaZeStajni),
-            isNotNull(zdarzeniaProfilaktyczne.dataWaznosci),
-          )
-        )
-        .orderBy(konie.nazwa, zdarzeniaProfilaktyczne.dataWaznosci);
-    }
-
-    for (const event of events) {
+    for (const event of _upcoming_events) {
       if (!userNotifications[event.email]) {
         userNotifications[event.email] = { wydarzenia: {} };
       }
@@ -112,7 +83,6 @@ export async function fetchUserEvents() {
         dataWaznosci: event.dataWaznosci ?? "",
       });
     }
-  }
 
   return userNotifications;
 }
