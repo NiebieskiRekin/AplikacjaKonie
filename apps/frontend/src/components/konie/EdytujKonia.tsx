e248ab1a1d933622b3e9f92413ce69dcf06b0e5d
Jakub Buler <jk.buler@gmail.com>
2025-04-24 10:33:16 +0200

    strzalka cofania w edycji konia

--- apps/frontend/src/components/konie/EdytujKonia.tsx
+++ apps/frontend/src/components/konie/EdytujKonia.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router";
 import { konie_plec_enum } from "@/frontend/types/types";
 import { BackendTypes } from "@aplikacja-konie/api-client";
+import { GoArrowLeft } from "react-icons/go";
 
 function EditKonia() {
   const { id } = useParams();
@@ -93,7 +94,18 @@ function EditKonia() {
 
   return (
     <div className="to-brown-600 flex min-h-screen flex-col items-center bg-gradient-to-br from-green-800 p-6">
-      <h2 className="mb-6 text-3xl font-bold text-white">Edytuj dane konia</h2>
+      <div className="relative mb-10 flex w-full max-w-7xl items-center justify-center sm:mb-6">
+        <button
+          onClick={() => void navigate("/konie")}
+          className="absolute left-0 flex items-center gap-2 rounded-lg bg-gradient-to-r from-gray-500 to-gray-700 px-4 py-2 text-white transition sm:relative sm:mr-auto"
+        >
+          <GoArrowLeft className="text-xl" />
+        </button>
+
+        <h2 className="absolute left-1/2 -translate-x-1/2 transform text-center text-3xl font-bold text-white">
+          Edytuj dane konia
+        </h2>
+      </div>
 
       {error && <p className="text-red-600">{error}</p>}
       {success && <p className="text-green-400">{success}</p>}
