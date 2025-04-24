f1414d49f1a9412e6df9c7894ec23c23102b110c
Jakub Buler <jk.buler@gmail.com>
2025-04-24 10:25:32 +0200

    poprawka usuwania konia

--- apps/frontend/src/components/konie/SzczegolyKonia.tsx
+++ apps/frontend/src/components/konie/SzczegolyKonia.tsx
@@ -534,7 +534,7 @@ function KonieDetails() {
               </button>
               <button
                 className="rounded-lg bg-red-600 px-4 py-2 text-white transition hover:bg-red-700"
-                onClick={() => handleDeleteHorse}
+                onClick={() => void handleDeleteHorse()}
               >
                 Usuń
               </button>
