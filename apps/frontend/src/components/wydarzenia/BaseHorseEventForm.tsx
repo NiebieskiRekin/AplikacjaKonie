eda28ecd13a350678162b3983093275fdc7db0d7
Jakub Buler <jk.buler@gmail.com>
2025-04-25 22:32:31 +0200

    dodanie strzalki do dodawania i edycji wydarzen per kon

--- apps/frontend/src/components/wydarzenia/BaseHorseEventForm.tsx
+++ apps/frontend/src/components/wydarzenia/BaseHorseEventForm.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { APIClient } from "@/frontend/lib/api-client";
+import { GoArrowLeft } from "react-icons/go";
 
 type EventFormProps = {
   id: string;
@@ -312,7 +313,20 @@ const BaseHorseEventForm = ({
 
   return (
     <div className="to-brown-600 flex min-h-screen flex-col items-center bg-gradient-to-br from-green-800 p-6">
-      <h2 className="mb-6 text-3xl font-bold text-white">✏ {title}</h2>
+      <div className="relative mb-10 flex w-full max-w-7xl items-center justify-center sm:mb-6">
+        <button
+          onClick={() => void navigate(`/wydarzenia/${id}/${type}`)}
+          className="absolute left-0 flex items-center gap-2 rounded-lg bg-gradient-to-r from-gray-500 to-gray-700 px-4 py-2 text-white transition sm:relative sm:mr-auto"
+        >
+          <GoArrowLeft className="text-xl" />
+        </button>
+
+        <h2 className="absolute left-1/2 -translate-x-1/2 transform text-center text-3xl font-bold text-white">
+          ✏ {title}
+        </h2>
+      </div>
+
+      {/* <h2 className="mb-6 text-3xl font-bold text-white"></h2> */}
 
       {error && <p className="text-red-600">{error}</p>}
       {success && <p className="text-green-500">{success}</p>}
