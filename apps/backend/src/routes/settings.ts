10958cab260983266248c76930f008d3a04293ee
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-11 17:35:19 +0200

    backend: update openapi spec to for /ustawienia

--- apps/backend/src/routes/settings.ts
+++ apps/backend/src/routes/settings.ts
@@ -8,8 +8,15 @@ import {
 } from "../middleware/auth";
 import { zValidator } from "@hono/zod-validator";
 import { eq, and, asc } from "drizzle-orm";
-import { RodzajPowiadomienia, RodzajWysylaniaPowiadomienia } from "../db/types";
+import {
+  RodzajPowiadomienia,
+  RodzajWysylaniaPowiadomienia,
+  RodzajePowiadomien,
+} from "../db/types";
 import { z } from "zod";
+import { describeRoute } from "hono-openapi";
+import { JsonMime, response_failure_schema } from "./constants";
+import { resolver } from "hono-openapi/zod";
 
 const common_settings = z.object({
   days: z.number().int().nonnegative(),
@@ -18,138 +25,170 @@ const common_settings = z.object({
   rodzajWysylania: z.enum(["Push", "Email", "Oba"]),
 });
 
-export const notificationsInsertSchema = z.object({
-  Podkucia: common_settings,
-  Odrobaczanie: common_settings,
-  "Podanie suplementów": common_settings,
-  Szczepienie: common_settings,
-  Dentysta: common_settings,
-  Inne: common_settings,
-});
+const settings_schema = z.record(z.enum(RodzajePowiadomien), common_settings);
 
 const settingsRoute = new Hono<{ Variables: { jwtPayload: UserPayload } }>()
   .use(authMiddleware)
-  .get("/", async (c) => {
-    try {
-      const userId = getUserFromContext(c);
-      if (!userId) return c.json({ error: "Błąd autoryzacji" }, 401);
-
-      const settings = await db
-        .select()
-        .from(notifications)
-        .where(eq(notifications.userId, userId))
-        .orderBy(asc(notifications.rodzajZdarzenia));
-
-      type pow = RodzajWysylaniaPowiadomienia;
-
-      const formattedSettings = settings.reduce(
-        (acc, setting) => {
-          acc[setting.rodzajZdarzenia] = {
-            days: setting.days,
-            time: setting.time.slice(0, 5),
-            active: setting.active,
-            rodzajWysylania: setting.rodzajWysylania,
-          };
-          return acc;
+  .get(
+    "/",
+    describeRoute({
+      description: "Wyświetl ustawienia powiadomień użytkownika",
+      responses: {
+        200: {
+          description: "Pomyślne zapytanie",
+          content: {
+            [JsonMime]: { schema: resolver(settings_schema) },
+          },
         },
-        {} as Record<
-          RodzajPowiadomienia,
-          { days: number; time: string; active: boolean; rodzajWysylania: pow }
-        >
-      );
+        401: {
+          description: "Bład autoryzacji",
+          content: {
+            [JsonMime]: { schema: resolver(response_failure_schema) },
+          },
+        },
+        500: {
+          description: "Bład serwera",
+          content: {
+            [JsonMime]: { schema: resolver(response_failure_schema) },
+          },
+        },
+      },
+    }),
+    async (c) => {
+      try {
+        const userId = getUserFromContext(c);
+        if (!userId) return c.json({ error: "Błąd autoryzacji" }, 401);
 
-      return c.json(formattedSettings, 200);
-    } catch (error) {
-      console.error("Błąd pobierania ustawień:", error);
-      return c.json({ error: "Błąd pobierania ustawień" }, 500);
+        const settings = await db
+          .select()
+          .from(notifications)
+          .where(eq(notifications.userId, userId))
+          .orderBy(asc(notifications.rodzajZdarzenia));
+
+        type pow = RodzajWysylaniaPowiadomienia;
+
+        const formattedSettings = settings.reduce(
+          (acc, setting) => {
+            acc[setting.rodzajZdarzenia] = {
+              days: setting.days,
+              time: setting.time.slice(0, 5),
+              active: setting.active,
+              rodzajWysylania: setting.rodzajWysylania,
+            };
+            return acc;
+          },
+          {} as Record<
+            RodzajPowiadomienia,
+            {
+              days: number;
+              time: string;
+              active: boolean;
+              rodzajWysylania: pow;
+            }
+          >
+        );
+
+        return c.json(formattedSettings, 200);
+      } catch (error) {
+        console.error("Błąd pobierania ustawień:", error);
+        return c.json({ error: "Błąd pobierania ustawień" }, 500);
+      }
     }
-  })
-  .put("/", zValidator("json", notificationsInsertSchema), async (c) => {
-    try {
-      const userId = getUserFromContext(c);
-      if (!userId) return c.json({ error: "Błąd autoryzacji" }, 401);
+  )
+  .put(
+    "/",
+    zValidator("json", settings_schema),
+    describeRoute({
+      description: "Zmień ustawienia powiadomień użytkownika",
+      responses: {
+        200: {
+          description: "Pomyślne zapytanie",
+          content: {
+            [JsonMime]: { schema: resolver(z.object({ message: z.string() })) },
+          },
+        },
+        401: {
+          description: "Bład autoryzacji",
+          content: {
+            [JsonMime]: { schema: resolver(response_failure_schema) },
+          },
+        },
+        500: {
+          description: "Bład serwera",
+          content: {
+            [JsonMime]: { schema: resolver(response_failure_schema) },
+          },
+        },
+      },
+    }),
+    async (c) => {
+      try {
+        const userId = getUserFromContext(c);
+        if (!userId) return c.json({ error: "Błąd autoryzacji" }, 401);
 
-      const settingsObject = c.req.valid("json");
+        const settingsObject = c.req.valid("json");
 
-      // TODO: refactor
-      await Promise.all(
-        Object.entries(settingsObject).map(
-          async ([rodzajZdarzenia, values]) => {
-            const existingSetting = await db
-              .select()
-              .from(notifications)
-              .where(
-                and(
-                  eq(notifications.userId, userId),
-                  eq(
-                    notifications.rodzajZdarzenia,
-                    rodzajZdarzenia as
-                      | "Podkucia"
-                      | "Odrobaczanie"
-                      | "Podanie suplementów"
-                      | "Szczepienie"
-                      | "Dentysta"
-                      | "Inne"
-                  )
-                )
-              )
-              .then((res) => res[0]);
-
-            if (existingSetting) {
-              await db
-                .update(notifications)
-                .set({
-                  days: Number(values.days),
-                  time: values.time,
-                  active: Boolean(values.active),
-                  rodzajWysylania: values.rodzajWysylania,
-                })
+        // TODO: refactor
+        await Promise.all(
+          Object.entries(settingsObject).map(
+            async ([rodzajZdarzenia, values]) => {
+              const existingSetting = await db
+                .select()
+                .from(notifications)
                 .where(
                   and(
                     eq(notifications.userId, userId),
                     eq(
                       notifications.rodzajZdarzenia,
-                      rodzajZdarzenia as
-                        | "Podkucia"
-                        | "Odrobaczanie"
-                        | "Podanie suplementów"
-                        | "Szczepienie"
-                        | "Dentysta"
-                        | "Inne"
+                      rodzajZdarzenia as RodzajPowiadomienia
                     )
                   )
-                );
-            } else {
-              await db.insert(notifications).values({
-                userId,
-                rodzajZdarzenia: rodzajZdarzenia as
-                  | "Podkucia"
-                  | "Odrobaczanie"
-                  | "Podanie suplementów"
-                  | "Szczepienie"
-                  | "Dentysta"
-                  | "Inne",
-                days: Number(values.days),
-                time: values.time,
-                active: Boolean(values.active),
-                rodzajWysylania: values.rodzajWysylania,
-              });
-            }
-          }
-        )
-      );
+                )
+                .then((res) => res[0]);
 
-      return c.json(
-        {
-          success: true,
-          message: "Ustawienia zostały zaktualizowane!",
-        },
-        200
-      );
-    } catch (error) {
-      console.error("Błąd aktualizacji ustawień:", error);
-      return c.json({ error: "Błąd aktualizacji ustawień" }, 500);
+              if (existingSetting) {
+                await db
+                  .update(notifications)
+                  .set({
+                    days: Number(values.days),
+                    time: values.time,
+                    active: Boolean(values.active),
+                    rodzajWysylania: values.rodzajWysylania,
+                  })
+                  .where(
+                    and(
+                      eq(notifications.userId, userId),
+                      eq(
+                        notifications.rodzajZdarzenia,
+                        rodzajZdarzenia as RodzajPowiadomienia
+                      )
+                    )
+                  );
+              } else {
+                await db.insert(notifications).values({
+                  userId,
+                  rodzajZdarzenia: rodzajZdarzenia as RodzajPowiadomienia,
+                  days: Number(values.days),
+                  time: values.time,
+                  active: Boolean(values.active),
+                  rodzajWysylania: values.rodzajWysylania,
+                });
+              }
+            }
+          )
+        );
+
+        return c.json(
+          {
+            message: "Ustawienia zostały zaktualizowane!",
+          },
+          200
+        );
+      } catch (error) {
+        console.error("Błąd aktualizacji ustawień:", error);
+        return c.json({ error: "Błąd aktualizacji ustawień" }, 500);
+      }
     }
-  });
+  );
 
 export default settingsRoute;
