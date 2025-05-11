318be2b03eabe1122c997fbb62777caf3777ad48
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-11 14:34:59 +0200

    backend: improve openapi spec for GET /healthcheck

--- apps/backend/src/routes/healthcheck.ts
+++ apps/backend/src/routes/healthcheck.ts
@@ -2,11 +2,39 @@ import { Hono } from "hono";
 import { db } from "../db";
 import { sql } from "drizzle-orm";
 
-export const healthcheck = new Hono().get("/", async (c) => {
-  try {
-    await db.execute(sql`select CURRENT_TIME`);
-    return c.json({ status: "ok" }, 200);
-  } catch {
-    return c.json({ status: "error" }, 500);
+import { describeRoute } from "hono-openapi";
+import { JsonMime } from "@/backend/routes/constants";
+import { resolver } from "hono-openapi/zod";
+import "@hono/zod-openapi";
+import { z } from "zod";
+
+const schema = z.object({ status: z.string() });
+
+export const healthcheck = new Hono().get(
+  "/",
+  describeRoute({
+    description: "Sprawdź stan usługi API",
+    responses: {
+      200: {
+        description: "Pomyślne zapytanie",
+        content: {
+          [JsonMime]: { schema: resolver(schema) },
+        },
+      },
+      500: {
+        description: "Bład serwera",
+        content: {
+          [JsonMime]: { schema: resolver(schema) },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      await db.execute(sql`select CURRENT_TIME`);
+      return c.json({ status: "ok" }, 200);
+    } catch {
+      return c.json({ status: "error" }, 500);
+    }
   }
-});
+);
