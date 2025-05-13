ffc2adcdc4cadda0149a0577c9ca0efd949cadb9
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-13 16:15:51 +0200

    backend: update openapi spec for /wydarzenia/leczenia

--- apps/backend/src/routes/wydarzenia/leczenia/post.ts
+++ apps/backend/src/routes/wydarzenia/leczenia/post.ts
@@ -1,25 +1,53 @@
 import { db } from "@/backend/db";
-import { leczenia, leczeniaInsertSchema } from "@/backend/db/schema";
+import {
+  leczenia,
+  leczeniaInsertSchema,
+  leczeniaSelectSchema,
+} from "@/backend/db/schema";
 import { Hono } from "hono";
 import { UserPayload } from "@/backend/middleware/auth";
-// import { describeRoute } from "hono-openapi";
-// import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
-// import { resolver } from "hono-openapi/zod";
+import { describeRoute } from "hono-openapi";
+import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
+import { resolver } from "hono-openapi/zod";
 import "@hono/zod-openapi";
 import { zValidator } from "@hono/zod-validator";
-// import { z } from "@hono/zod-openapi";
 
 export const wydarzenia_leczenia_post = new Hono<{
   Variables: { jwtPayload: UserPayload };
-}>().post("/leczenia", zValidator("json", leczeniaInsertSchema), async (c) => {
-  const _leczenia = c.req.valid("json");
-  _leczenia.kon = Number(_leczenia.kon);
+}>().post(
+  "/leczenia",
+  zValidator("json", leczeniaInsertSchema),
+  describeRoute({
+    description: "Dodaj nowe podkucie",
+    responses: {
+      201: {
+        description: "Pomyślne zapytanie",
+        content: {
+          [JsonMime]: { schema: resolver(leczeniaSelectSchema) },
+        },
+      },
+      500: {
+        description: "Bład serwera",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const _leczenia = c.req.valid("json");
+      _leczenia.kon = Number(_leczenia.kon);
 
-  const result = await db
-    .insert(leczenia)
-    .values(_leczenia)
-    .returning()
-    .then((res) => res[0]);
+      const result = await db
+        .insert(leczenia)
+        .values(_leczenia)
+        .returning()
+        .then((res) => res[0]);
 
-  return c.json(result, 201);
-});
+      return c.json(result, 201);
+    } catch {
+      return c.json({ error: "Błąd serwera" }, 500);
+    }
+  }
+);
--- apps/backend/src/routes/wydarzenia/leczenia/put.ts
+++ apps/backend/src/routes/wydarzenia/leczenia/put.ts
@@ -1,20 +1,49 @@
 import { db } from "@/backend/db";
-import { leczenia, leczeniaUpdateSchema } from "@/backend/db/schema";
+import {
+  leczenia,
+  leczeniaSelectSchema,
+  leczeniaUpdateSchema,
+} from "@/backend/db/schema";
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
 import { UserPayload } from "@/backend/middleware/auth";
-// import { describeRoute } from "hono-openapi";
-// import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
-// import { resolver } from "hono-openapi/zod";
+import { describeRoute } from "hono-openapi";
+import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
+import { resolver } from "hono-openapi/zod";
 import "@hono/zod-openapi";
 import { zValidator } from "@hono/zod-validator";
-// import { z } from "@hono/zod-openapi";
+import { z } from "@hono/zod-openapi";
+
+const successful_response = z.object({ updatedEvent: leczeniaSelectSchema });
 
 export const wydarzenia_leczenia_put = new Hono<{
   Variables: { jwtPayload: UserPayload };
 }>().put(
   "/leczenia/:id{[0-9]+}",
   zValidator("json", leczeniaUpdateSchema),
+  describeRoute({
+    description: "Zmień informacje o wskazanym podkuciu",
+    responses: {
+      200: {
+        description: "Pomyślne zapytanie",
+        content: {
+          [JsonMime]: { schema: resolver(successful_response) },
+        },
+      },
+      400: {
+        description: "Błąd zapytania",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+      500: {
+        description: "Bład serwera",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+    },
+  }),
   async (c) => {
     const eventId = Number(c.req.param("id"));
     const updatedData = c.req.valid("json");
@@ -36,7 +65,7 @@ export const wydarzenia_leczenia_put = new Hono<{
         );
       }
 
-      return c.json({ success: true, updatedEvent: updateQuery[0] }, 200);
+      return c.json({ updatedEvent: updateQuery[0] }, 200);
     } catch (error) {
       console.error("Błąd aktualizacji wydarzenia:", error);
       return c.json({ error: "Błąd aktualizacji wydarzenia" }, 500);
