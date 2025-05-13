5f71f6d026e27d672cffb2424e9d48104c7fdece
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-13 15:58:30 +0200

    backend: update openapi spec for /wydarzenia/rozrody

--- apps/backend/src/routes/wydarzenia/rozrody/post.ts
+++ apps/backend/src/routes/wydarzenia/rozrody/post.ts
@@ -1,27 +1,54 @@
 import { db } from "@/backend/db";
-import { rozrody, rozrodyInsertSchema } from "@/backend/db/schema";
+import {
+  rozrody,
+  rozrodyInsertSchema,
+  rozrodySelectSchema,
+} from "@/backend/db/schema";
 import { Hono } from "hono";
 import { UserPayload } from "@/backend/middleware/auth";
-// import { describeRoute } from "hono-openapi";
-// import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
-// import { resolver } from "hono-openapi/zod";
+import { describeRoute } from "hono-openapi";
+import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
+import { resolver } from "hono-openapi/zod";
 import "@hono/zod-openapi";
 import { zValidator } from "@hono/zod-validator";
-// import { z } from "@hono/zod-openapi";
 
 export const wydarzenia_rozrody_post = new Hono<{
   Variables: { jwtPayload: UserPayload };
-}>().post("/rozrody", zValidator("json", rozrodyInsertSchema), async (c) => {
-  const _rozrody = c.req.valid("json");
-  console.log(_rozrody);
-  _rozrody.kon = Number(_rozrody.kon);
-  _rozrody.weterynarz = Number(_rozrody.weterynarz);
+}>().post(
+  "/rozrody",
+  zValidator("json", rozrodyInsertSchema),
+  describeRoute({
+    description: "Dodaj wydarzenie rozrodcze",
+    responses: {
+      200: {
+        description: "Pomyślne zapytanie",
+        content: {
+          [JsonMime]: { schema: resolver(rozrodySelectSchema) },
+        },
+      },
+      500: {
+        description: "Bład serwera",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const _rozrody = c.req.valid("json");
+      _rozrody.kon = Number(_rozrody.kon);
+      _rozrody.weterynarz = Number(_rozrody.weterynarz);
 
-  const result = await db
-    .insert(rozrody)
-    .values(_rozrody)
-    .returning()
-    .then((res) => res[0]);
+      const result = await db
+        .insert(rozrody)
+        .values(_rozrody)
+        .returning()
+        .then((res) => res[0]);
 
-  return c.json(result, 201);
-});
+      return c.json(result, 200);
+    } catch {
+      return c.json({ error: "Błąd serwera" }, 500);
+    }
+  }
+);
--- apps/backend/src/routes/wydarzenia/rozrody/put.ts
+++ apps/backend/src/routes/wydarzenia/rozrody/put.ts
@@ -1,20 +1,51 @@
 import { db } from "@/backend/db";
-import { rozrody, rozrodyUpdateSchema } from "@/backend/db/schema";
+import {
+  rozrody,
+  rozrodySelectSchema,
+  rozrodyUpdateSchema,
+} from "@/backend/db/schema";
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
 import { UserPayload } from "@/backend/middleware/auth";
-// import { describeRoute } from "hono-openapi";
-// import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
-// import { resolver } from "hono-openapi/zod";
+import { describeRoute } from "hono-openapi";
+import { JsonMime, response_failure_schema } from "@/backend/routes/constants";
+import { resolver } from "hono-openapi/zod";
 import "@hono/zod-openapi";
 import { zValidator } from "@hono/zod-validator";
-// import { z } from "@hono/zod-openapi";
+import { z } from "@hono/zod-openapi";
+
+const successful_response = z.object({
+  updatedEvent: rozrodySelectSchema,
+});
 
 export const wydarzenia_rozrody_put = new Hono<{
   Variables: { jwtPayload: UserPayload };
 }>().put(
   "/rozrody/:id{[0-9]+}",
   zValidator("json", rozrodyUpdateSchema),
+  describeRoute({
+    description: "Zmień informacje wskazanego wydarzenia rozrodczego",
+    responses: {
+      200: {
+        description: "Pomyślne zapytanie",
+        content: {
+          [JsonMime]: { schema: resolver(successful_response) },
+        },
+      },
+      400: {
+        description: "Błąd zapytania",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+      500: {
+        description: "Bład serwera",
+        content: {
+          [JsonMime]: { schema: resolver(response_failure_schema) },
+        },
+      },
+    },
+  }),
   async (c) => {
     const eventId = Number(c.req.param("id"));
     const updatedData = c.req.valid("json");
@@ -36,7 +67,7 @@ export const wydarzenia_rozrody_put = new Hono<{
         );
       }
 
-      return c.json({ success: true, updatedEvent: updateQuery[0] }, 200);
+      return c.json({ updatedEvent: updateQuery[0] }, 200);
     } catch (error) {
       console.error("Błąd aktualizacji wydarzenia:", error);
       return c.json({ error: "Błąd aktualizacji wydarzenia" }, 500);
