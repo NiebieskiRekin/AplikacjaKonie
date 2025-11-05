4472e36afeeb30abe215709b6d36d08fe5efa446
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-11-05 14:02:58 +0100

    Basic better auth setup

---
+++ apps/backend/src/auth.ts
@@ -0,0 +1,44 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/backend/db";
+import { log } from "./logs/logger";
+import {
+  sendAccountConfirmationEmail,
+  sendResetPasswordEmail,
+} from "./mailer/accountMailer";
+import { bearer, jwt, openAPI, organization } from "better-auth/plugins";
+import { ProcessEnv } from "./env";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+    revokeSessionsOnPasswordReset: true,
+    sendResetPassword: async ({ user, url, token }) => {
+      log("Account", "info", "Password reset email " + token);
+      await sendResetPasswordEmail(user.email, user.name, url);
+    },
+    resetPasswordTokenExpiresIn: 24 * 60 * 60,
+  },
+  trustedOrigins: ProcessEnv.TRUSTED_ORIGINS,
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url, token }) => {
+      log("Account", "info", "Send verification email " + token);
+      await sendAccountConfirmationEmail(user.email, user.name, url);
+    },
+    expiresIn: 24 * 60 * 60,
+    autoSignInAfterVerification: true,
+    sendOnSignUp: true,
+  },
+  plugins: [bearer(), organization(), openAPI(), jwt()],
+});
+
+export type auth_vars = {
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+};
---
+++ apps/backend/src/middleware/auth-middleware.ts
@@ -0,0 +1,20 @@
+import { auth_vars } from "@/backend/lib/auth";
+import { MiddlewareHandler } from "hono";
+import { auth } from "../lib/auth";
+
+export const auth_middleware: MiddlewareHandler<auth_vars> = async (
+  c,
+  next
+) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+};
