d801be7e8c1ffc205c034385ce1457114dbe9251
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-12-31 15:47:44 +0100

    Allow unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -95,5 +95,14 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-call": "warn",
       "@typescript-eslint/no-unsafe-return": "warn",
     },
+  },
+  {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+    },
   }
 );
