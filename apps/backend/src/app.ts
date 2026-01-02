f3fb8f6a2e6869144d92aed5500a288e7fe000e3
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2026-01-02 02:51:55 +0100

    Fix really sneaky bug in Hono default router by enforcing a slower but consistent LinearRouter

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,8 +2,9 @@ import { Hono } from "hono";
 import { ProcessEnv } from "./env";
 import { registerRoutes } from "./routes";
 import { log } from "./logs/logger";
+import { LinearRouter } from "hono/router/linear-router";
 import "./crons/index";
-const app = registerRoutes(new Hono());
+const app = registerRoutes(new Hono({ router: new LinearRouter() }));
 
 log("Server", "info", "Mode: " + ProcessEnv.NODE_ENV);
 
